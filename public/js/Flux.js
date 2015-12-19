e0f9d80368eb4f804ca29c8041de1d2c0d6f7d05
Gabriel Alan <gaba.alan@gmail.com>
2015-12-18 23:26:41 -0200

    Adding sub-repo

--- public/js/Flux.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var Fluxxor = require('Fluxxor'),
-    EntryActions = require("Actions/EntryActions"),
-    EntryStore = require("Stores/EntryStore");
-
-module.exports = new Fluxxor.Flux(EntryStore, EntryActions.actions);
